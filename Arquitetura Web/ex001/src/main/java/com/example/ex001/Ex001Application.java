8ae7bc2ceb7dbdc76e91c8b7c5614fda24281942
viniciusalexandre2 <vinicius141103a@gmail.com>
2025-03-06 21:42:25 -0300

    Add files via upload

---
+++ Arquitetura Web/ex001/src/main/java/com/example/ex001/Ex001Application.java
@@ -0,0 +1,13 @@
+package com.example.ex001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex001Application.class, args);
+	}
+
+}
---
+++ Arquitetura Web/ex001/src/test/java/com/example/ex001/Ex001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
